3d58a7cf02a1df987f13b1ce1e3d45c5fb00d025
kirimaru <nainaistar@gmail.com>
2021-10-13 13:40:56 +0000

    add: 動かないテストは何もしあに

--- environment/sample_app/app/models/user.rb
+++ environment/sample_app/app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
 
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-    validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
+    validates :email, presence: true, length: { maximum: 255 } , format: { with: VALID_EMAIL_REGEX }
+    # , uniqueness: true
 
 
     def initialize(attributes = {})
