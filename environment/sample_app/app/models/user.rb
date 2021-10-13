c71203e9cfa888168a622298273355796f7f04f1
kirimaru <nainaistar@gmail.com>
2021-10-13 13:31:54 +0000

    add: validate

--- environment/sample_app/app/models/user.rb
+++ environment/sample_app/app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
     attr_accessor :name, :email
     validates :name, presence: true
+    validates :email, presence: true
+
 
     def initialize(attributes = {})
       @name  = attributes[:name]
