28562b24b40cc6585412dd64a55ad6e827f2b4d4
kirimaru <nainaistar@gmail.com>
2021-10-14 12:58:24 +0000

    add: ch7を上書き

--- environment/sample_app/app/controllers/application_controller.rb
+++ environment/sample_app/app/controllers/application_controller.rb
@@ -1,12 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  # def hello
-  #   render html: "hello, world!"
-  # end
-  def show
-    @user = User.find(params[:id])
-  end
 
-  def new
+  def hello
+    render html: 'hello, world!'
   end
 end
--- environment/sample_app/app/models/user.rb
+++ environment/sample_app/app/models/user.rb
@@ -1,22 +1,10 @@
 class User < ApplicationRecord
-    # before_save { email.downcase! }
-    # before_save { self.email = email.downcase }
-    attr_accessor :name, :email
-    validates :name, presence: true, length: { maximum: 50 }
-
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-
-    validates :email, presence: true, length: { maximum: 255 } , format: { with: VALID_EMAIL_REGEX }
-    # , uniqueness: true
-
-
-    def initialize(attributes = {})
-      @name  = attributes[:name]
-      @email = attributes[:email]
-    end
-  
-    def formatted_email
-      "#{@name} <#{@email}>"
-    end
-    # has_secure_password
-  end
+  validates :name,  presence: true, length: { maximum:  50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, presence: true,
+    length: { minimum: 6 }, allow_nil: true
+end
