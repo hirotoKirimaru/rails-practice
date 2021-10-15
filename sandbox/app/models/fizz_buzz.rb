94ef91554f217c8d4655eacc553bf13d4de35857
kirimaru <nainaistar@gmail.com>
2021-10-15 10:21:02 +0000

    1 to 1

--- sandbox/app/models/fizz_buzz.rb
+++ sandbox/app/models/fizz_buzz.rb
@@ -1,8 +1,8 @@
 class FizzBuzz
-    # def execute
-    # end
+    def execute
+    end
 
-    # def convert(value)
-    #   return "1"
-    # end
+    def convert(value)
+      return "1"
+    end
 end
--- sandbox/test/models/fizz_buzz_test.rb
+++ sandbox/test/models/fizz_buzz_test.rb
@@ -1,17 +1,13 @@
 require 'test_helper'
-# require './fizz_buzz'
 
 class FizzBuzzTest < ActiveSupport::TestCase
-  # def setup
-  #   @fizz_buzz = FizzBuzz.new
-  # end
+  def setup
+    @fizz_buzz = FizzBuzz.new
+  end
 
   test "1 to 1" do
-    # r = @fizz_buzz.convert 1
-    # assert_equal(1, r)
-    assert_equal("1", "1")
+    assert_equal("1", @fizz_buzz.convert(1))
   end 
-  # test "the truth" do
-  #   assert true
-  # end
+
+  
 end
