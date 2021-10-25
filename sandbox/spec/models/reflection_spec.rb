4f8f1afc191633a1e9a4eca2f4373f4b3e005963
kirimaru <nainaistar@gmail.com>
2021-10-25 14:03:50 +0000

    Argument error

--- sandbox/spec/models/reflection_spec.rb
+++ sandbox/spec/models/reflection_spec.rb
@@ -4,10 +4,30 @@ RSpec.describe 'リフレクションクラスの確認', type: :model do
   let(:reflection) {Reflection.new}
 
   describe 'リフレクションの確認' do
-    context 'houseを呼び出す' do
+    it "ArgumentError" do
+        expect do
+            reflection.public_send()
+        end.to raise_error(ArgumentError)
+    end 
+
+    context 'パラメータなし' do
         it "houseを呼び出す" do
             expect(reflection.public_send("house")).to eq "house"
         end 
-      end
+
+        it "mouseを呼び出す" do
+            expect(reflection.public_send("mouse")).to eq "mouse"
+        end 
+
+        it "noseを呼び出す" do
+            expect(reflection.public_send("nose")).to eq "nose"
+        end 
+
+        it "gorillaを呼び出す" do
+            expect do
+                reflection.public_send("gorilla")
+            end.to raise_error(NoMethodError)
+        end 
+    end
   end
 end
