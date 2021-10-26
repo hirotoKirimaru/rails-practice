790bafdea85093c95d0ad7a35c67f2aed9763062
kirimaru <nainaistar@gmail.com>
2021-10-26 13:58:19 +0000

    subjectの使い方覚える

--- sandbox/spec/models/fizzbuzz_spec.rb
+++ sandbox/spec/models/fizzbuzz_spec.rb
@@ -3,9 +3,26 @@ require 'rails_helper'
 RSpec.describe 'FizzBuzzクラスの確認', type: :model do
   let(:target) {FizzBuzz.new}
 
-  describe 'リフレクションの確認' do
-    it '1 to 1' do
-        expect(target.convert(1)).to eq "1"
+  describe 'FizzBuzzの確認' do
+
+    context 'subjectの動作確認' do
+       
+        subject {
+            target.convert(1)   
+        }
+
+        it '1 to 1' do
+            is_expected.to eq "1"
+        end
     end
+
+    context '普通のやりかた' do
+        it '1 to 1' do
+            expect(target.convert(1)).to eq "1"
+        end
+    end
+
   end
+
+
 end
