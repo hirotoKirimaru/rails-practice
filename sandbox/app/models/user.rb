d06213c21d1bbf071199d9d44e4c724d706287ce
kirimaru <nainaistar@gmail.com>
2021-11-22 12:20:10 +0000

    add: 仕込み

--- sandbox/app/models/user.rb
+++ sandbox/app/models/user.rb
@@ -1,10 +1,2 @@
 class User < ApplicationRecord
-    # def initialize()
-    #     @created_at = Time.now
-    #     @updated_at = Time.now
-    # end
-
-    # def eql?(obj)
-        
-    # end
 end
--- sandbox/spec/models/user_spec.rb
+++ sandbox/spec/models/user_spec.rb
@@ -3,12 +3,16 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   context 'ユーザクラスの作成' do
     it "同一作成時間になること" do
+        # freeze_time
+
         old_user = User.new(id: 1, name: 'name', email: 'test@example.com', created_at: Time.now, updated_at: Time.now)
-        new_user = User.new(id: 1, name: 'name', email: 'test@example.com')
+        new_user = User.new(id: 1, name: 'name', email: 'test@example.com', created_at: Time.now, updated_at: Time.now)
 
         expect(old_user.id).to eq(new_user.id)
         expect(old_user.name).to eq(new_user.name)
         expect(old_user.email).to eq(new_user.email)
+        # expect(old_user.created_at).to eq(new_user.created_at)
+        # expect(old_user.updated_at).to eq(new_user.updated_at)
     end 
   end
 end
