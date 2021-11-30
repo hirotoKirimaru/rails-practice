de0ed4210b2ef58e74d5fed7098585f5b0709350
kirimaru <nainaistar@gmail.com>
2021-11-30 14:02:44 +0000

    add

--- sandbox/spec/models/user_spec.rb
+++ sandbox/spec/models/user_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before do
+    freeze_time
+  end
+
   context 'ユーザクラスの作成' do
     it "同一作成時間になること" do
         # freeze_time
@@ -11,8 +15,8 @@ RSpec.describe User, type: :model do
         expect(old_user.id).to eq(new_user.id)
         expect(old_user.name).to eq(new_user.name)
         expect(old_user.email).to eq(new_user.email)
-        # expect(old_user.created_at).to eq(new_user.created_at)
-        # expect(old_user.updated_at).to eq(new_user.updated_at)
+        expect(old_user.created_at).to eq(new_user.created_at)
+        expect(old_user.updated_at).to eq(new_user.updated_at)
     end 
   end
 end
