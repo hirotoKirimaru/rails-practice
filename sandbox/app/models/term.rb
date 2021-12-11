e4e556272dad01c34c1dc228626466174c2e8edb
kirimaru <nainaistar@gmail.com>
2021-12-11 13:49:08 +0000

    docs

--- sandbox/app/models/term.rb
+++ sandbox/app/models/term.rb
@@ -1,7 +1,8 @@
 class Term
+  # TODO: 型をどこかで表現したい
   attr_reader :start_date
   attr_reader :end_date
-  
+
   def initialize(start_date: Time.new, end_date: Time.new)
     @start_date = start_date
     @end_date = end_date
