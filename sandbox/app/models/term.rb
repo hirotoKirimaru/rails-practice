8639457f88eaf678c89953cff84b2c8f0ed9eed5
kirimaru <nainaistar@gmail.com>
2021-12-11 13:43:03 +0000

    attr_reader付与

--- sandbox/app/models/term.rb
+++ sandbox/app/models/term.rb
@@ -1,10 +1,10 @@
 class Term
-  @start_date
-  @end_date 
-
+  attr_reader :start_date
+  attr_reader :end_date
+  
   def initialize(start_date: Time.new, end_date: Time.new)
-    @end = start_date
-    @end = end_date
+    @start_date = start_date
+    @end_date = end_date
   end
 
   def is_not_daily_rate_term?
