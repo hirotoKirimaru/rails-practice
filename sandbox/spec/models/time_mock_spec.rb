9d2ea5cbf1a79617ce12ea3c74afb0bc8968e667
水上 皓登 <nainaistar@gmail.com>
2021-12-27 21:09:54 +0900

    feat: 時間周りのモックを検証しておく

--- sandbox/spec/models/time_mock_spec.rb
+++ sandbox/spec/models/time_mock_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe 'Timeクラスの動作確認', type: :model do
+  let(:_20200101) { Time.new(2020, 1, 1, 0, 0, 0) }
+  let(:_20210101) { Time.new(2021, 1, 1, 0, 0, 0) }
+
   describe "呼び出す回数ごとに処理する内容を変更する" do
-    let(:_20200101) { Time.new(2020, 1, 1, 0, 0, 0) }
-    let(:_20210101) { Time.new(2021, 1, 1, 0, 0, 0) }
     before do
       allow(Time).to receive(:now)
                        .and_return(_20200101, _20210101)
@@ -15,7 +16,6 @@ RSpec.describe 'Timeクラスの動作確認', type: :model do
   end
   
   describe "Time.nowのモック(receive_message_chain)" do
-    let(:_20200101) { Time.new(2020, 1, 1, 0, 0, 0) }
     before do
       allow(Time).to receive_message_chain(:now)
                        .and_return(_20200101)
@@ -26,7 +26,6 @@ RSpec.describe 'Timeクラスの動作確認', type: :model do
   end
 
   describe "Time.nowのモック(receive)" do
-    let(:_20200101) { Time.new(2020, 1, 1, 0, 0, 0) }
     before do
       allow(Time).to receive(:now)
                        .and_return(_20200101)
@@ -41,6 +40,19 @@ RSpec.describe 'Timeクラスの動作確認', type: :model do
     before { freeze_time }
     it "同一作成時間になること" do
       expect(Time.now).to eq(Time.now)
+    end
+  end
+
+  context 'travelとtravel_to' do
+    before { travel_to(_20200101)}
+    it "同一作成時間になること" do
+      now = Time.now
+      travel 1.years
+      now2 = Time.now
+      
+      expect(now).not_to eq(now2)
+      expect(now).to eq(_20200101)
+      expect(now2).to eq(_20210101)
     end
   end
 end
