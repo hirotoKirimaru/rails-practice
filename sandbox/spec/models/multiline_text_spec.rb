d38cd0980ef0109f5c5ae2d20bde5ddd17b3967e
水上 皓登 <nainaistar@gmail.com>
2022-01-03 11:56:49 +0900

    feat: ブロック文

--- sandbox/spec/models/multiline_text_spec.rb
+++ sandbox/spec/models/multiline_text_spec.rb
@@ -19,6 +19,23 @@ RSpec.describe '複数行文字列の動作確認', type: :model do
     expect(actual).to eq expected
   end
 
+  it '%{と<<~EOSの挙動確認' do
+    actual =
+      %{aiueo xyz
+12345 EOS
+}
+
+    expected = <<~EOS
+      aiueo xyz
+      12345 EOS
+    EOS
+
+    p actual
+    p expected
+
+    expect(actual).to eq expected
+  end
+
   it '%|と<<~EOSの挙動確認(not)' do
     actual = %|
       aiueo xyz
